13d5dae4eb5361a9e7721d543df38ac5fce681d8
jon <jon710505@gmail.com>
2024-06-18 15:16:08 -0500

    feat: buscar numero

--- test01.dart
+++ test01.dart
@@ -1,3 +1,10 @@
 void main(List<String> args) {
   print('hola mundo');
+
+  List<int> numeros= [1,2,3,4,5];
+  for (var i = 0; i < numeros.length; i++) {
+    print(numeros[i]);
+  }
+  // buscar numero
+  int num = 3;
 }
