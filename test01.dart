ad55b8a70c3290842e1b006a4796b2c6bbe69900
jon <jon710505@gmail.com>
2024-06-18 16:55:12 -0500

    feat:ciudades

--- test01.dart
+++ test01.dart
@@ -1,10 +1,32 @@
+import 'dart:io';
+
 void main(List<String> args) {
   print('hola mundo');
 
-  List<int> numeros= [1,2,3,4,5];
+  List<int> numeros = [1, 2, 3, 4, 5];
   for (var i = 0; i < numeros.length; i++) {
-    print(numeros[i]);
+    stdout.write(numeros[i]);
   }
   // buscar numero
-  int num = 3;
+  int num = 3, ele = 0, pos = 0;
+  for (var i = 0; i < numeros.length; i++) {
+    if (numeros[i] == num) {
+      ele = numeros[i];
+      pos = i;
+    }
+  }
+
+  print('\nel elemento $ele se encuentra en la posicion $pos');
+
+  var ciudades = {
+    'Mexico': 'CDMX',
+    'Argentina': 'Buenos Aires',
+    'España': 'Madrid',
+    'Colombia': 'Bogota'
+  };
+  ciudades['Chile'] = 'Santiago de Chile';
+  var ciudades2 = {'Uruguay': 'Montevideo'};
+  ciudades.addAll(ciudades2);
+
+  print(ciudades);
 }
---
+++ test02.dart
@@ -0,0 +1,18 @@
+main(List<String> arguments) {
+  var operacion1 = operacion(5, 3, suma);
+  var operacion2 = operacion(5, 3, resta);
+  print(operacion1);
+  print(operacion2);
+}
+
+int operacion(int a, int b, Function func) {
+  return func(a, b);
+}
+
+int suma(int a, int b) {
+  return a + b;
+}
+
+int resta(int a, int b) {
+  return a - b;
+}
